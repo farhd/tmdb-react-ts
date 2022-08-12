9422214601b0f6b339957045e6cbadd71bb41e41
Farhad Abdullayev <farhad.abdullayev@emteria.com>
2022-08-12 03:33:07 +0200

    add smoke test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("it renders app", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/The Movie Database/i);
   expect(linkElement).toBeInTheDocument();
 });
